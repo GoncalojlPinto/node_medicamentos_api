5df346b273bc253383620e6ff91c1f2773682228
Goncalo Pinto <goncalojlpinto@gmail.com>
2021-10-02 17:02:33 +0100

    basic CRUD created

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ app.get('/', (req, res) => {
   res.send('Welcome do Medicine API')});
 
 app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT ${PORT} `))
+
+//MOCKUP de API
+
+app.get('/medicines/', (req, res) => {
+  return res.json({brand: 'ben-u-ron', drug: 'paracetamol'}).status(200);
+});
+
+app.get('/medicines/:id', (req, res) => {
+  res.send('retorna 1 Medicamento')
+});
+
+app.post('/medicines/', (req, res) => {
+  console.log(req.params.id)
+  res.send('POsta um medicamento')
+});
+
+app.put('/medicines/:1', (req, res) => {
+  res.send('Altera 1 medicamento')
+});
+
+app.delete('/medicines/:id', (req, res) => {
+  res.send('retorna todos os medicamentos')
+});
